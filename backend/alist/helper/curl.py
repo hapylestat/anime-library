1f82fbbf62f9dc14c9ed93907c0108a09fe5d4a3
Reishin <hapylestat@users.noreply.github.com>
2015-08-12 09:23:41 +0300

    Update curl.py

--- backend/alist/helper/curl.py
+++ backend/alist/helper/curl.py
@@ -3,7 +3,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
-# Copyright (c) 2015 Reishin <hapy.lestat@gmail.com> and Contributors
+# Copyright (c) 2015 Reishin <hapy.lestat@gmail.com>
 
 
 import sys
